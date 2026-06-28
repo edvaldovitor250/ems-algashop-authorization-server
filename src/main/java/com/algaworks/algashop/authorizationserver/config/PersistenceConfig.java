6739aad174b789396782ecbc3108fcd57d8225a8
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-06-28 16:45:44 -0300

    Armazenamento de consentimento do usuário

--- src/main/java/com/algaworks/algashop/authorizationserver/config/PersistenceConfig.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/config/PersistenceConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algashop.authorizationserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 
@@ -10,10 +11,15 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 public class PersistenceConfig {
 
 	@Bean
-	public JdbcOAuth2AuthorizationService authorizationService(
-			JdbcTemplate jdbcTemplate,
+	public JdbcOAuth2AuthorizationService authorizationService(JdbcTemplate jdbcTemplate,
 			RegisteredClientRepository registeredClientRepository) {
 		return new JdbcOAuth2AuthorizationService(jdbcTemplate, registeredClientRepository);
+	}
+
+	@Bean
+	JdbcOAuth2AuthorizationConsentService authorizationConsentService(JdbcTemplate jdbcTemplate,
+			RegisteredClientRepository registeredClientRepository) {
+		return new JdbcOAuth2AuthorizationConsentService(jdbcTemplate, registeredClientRepository);
 	}
 
 }
