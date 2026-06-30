ac0de69495da00b3af3def98a7307e35d1b6636c
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-06-30 20:37:02 -0300

    Iniciando implementação da REST API de usuários

---
+++ src/main/java/com/algaworks/algashop/presentation/MyUserController.java
@@ -0,0 +1,12 @@
+package com.algaworks.algashop.presentation;
+
+@RestController
+@RequestMapping("/api/v1/users/me")
+public class MyUserController {
+
+    @GetMapping
+    public void getMe(){
+        
+    }
+    
+}
