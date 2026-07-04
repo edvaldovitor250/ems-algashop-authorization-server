bd68779e0e8592cbab114131bc6a5ff5dfd6ceee
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-07-04 11:03:36 -0300

    Preparando Authorization Server para permitir Silent Refresh

--- src/main/java/com/algaworks/algashop/infrastructure/security/AlgaShopSecurityProperties.java
+++ src/main/java/com/algaworks/algashop/infrastructure/security/AlgaShopSecurityProperties.java
@@ -1,21 +1,59 @@
-package com.algaworks.algashop.infrastructure.security;
+package com.algaworks.algashop.authorizationserver.infrastructure.security;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Data
 @Validated
-@ConfigurationProperties(prefix = "algashop.security")
+@ConfigurationProperties("algashop.security")
 @NoArgsConstructor
 public class AlgaShopSecurityProperties {
 
-    @NotNull
-    @Valid
-    private CorsProperties cors;
+	@NotNull
+	@Valid
+	private CorsProperties cors;
 
-    @Data
-    @NoArgsConstructor
-public static class CorsProperties{
-    @NotNull
-    private List<String> allowedOrigins = new ArrayList<>();
-}
+	@NotNull
+	@Valid
+	private CookieProperties cookie;
+
+	@NotNull
+	@Valid
+	private CspProperties csp;
+
+	@Data
+	@NoArgsConstructor
+	public static class CorsProperties {
+		@NotEmpty
+		private List<String> allowedOrigins = new ArrayList<>();
+	}
+
+	@Data
+	@NoArgsConstructor
+	public static class CookieProperties {
+		@NotBlank
+		private String sameSite;
+		@NotBlank
+		private String domainName;
+		@NotNull
+		private Boolean secure;
+	}
+
+	@Data
+	@NoArgsConstructor
+	public static class CspProperties {
+		private String policyDirectives;
+	}
 
 }
--- src/main/java/com/algaworks/algashop/infrastructure/security/cors/CorsConfig.java
+++ src/main/java/com/algaworks/algashop/infrastructure/security/cors/CorsConfig.java
@@ -1,18 +1,29 @@
-package com.algaworks.algashop.infrastructure.security.cors;
+package com.algaworks.algashop.authorizationserver.infrastructure.security.cors;
+
+import com.algaworks.algashop.authorizationserver.infrastructure.security.AlgaShopSecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
 
-    public CorsConfigurationSource corsConfigurationSource(AlgaShopSecurityProperties securityProperties) {
-        CorsConfiguration configuration = new CorsConfiguration();
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(AlgaShopSecurityProperties properties) {
+		CorsConfiguration cors = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(securityProperties.getCors().getAllowedOrigins());
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-    
+		cors.setAllowedOrigins(properties.getCors().getAllowedOrigins());
+		cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		cors.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		cors.setAllowCredentials(true);
+
+		var source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", cors);
+		return source;
+	}
+
 }
