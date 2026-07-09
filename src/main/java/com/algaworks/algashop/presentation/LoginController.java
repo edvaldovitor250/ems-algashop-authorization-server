01e17b2369371c875fad56a51da858fb3669119e
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-07-08 21:06:29 -0300

    Customizando tela login

---
+++ src/main/java/com/algaworks/algashop/presentation/LoginController.java
@@ -0,0 +1,17 @@
+package com.algaworks.algashop.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    
+    
+}
