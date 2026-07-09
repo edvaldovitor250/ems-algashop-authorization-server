ff95879fe3cf2951ae9625baf3a300c2f3cc697c
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-07-08 21:17:56 -0300

    Customizando tela logout

---
+++ src/main/java/com/algaworks/algashop/presentation/LogoutController.java
@@ -0,0 +1,11 @@
+package com.algaworks.algashop.presentation;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LogoutController {
+    public String logout() {
+        return "logout";
+    }
+    
+}
