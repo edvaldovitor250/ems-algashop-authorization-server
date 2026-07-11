8f74d9496b5a65971d163799a62e4d2260d2ce08
Edvaldo Vitor <edvaldovitor250@gmail.com>
2026-07-11 11:00:07 -0300

    Envio de email assíncrono

---
+++ src/main/java/com/algaworks/algashop/infrastructure/async/AsyncConfig.java
@@ -0,0 +1,7 @@
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configurable
+@EnableAsync
+public class AsyncConfig {
+}
